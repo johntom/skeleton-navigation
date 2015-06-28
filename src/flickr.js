f0eec4d86d1c1312d8132d0a68da0231ca04875d
John Tomaselli <jrt@gtz.com>
2015-06-28 12:13:52 -0400

    y first auriela repo

--- src/flickr.js
+++ src/flickr.js
@@ -2,18 +2,31 @@ import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 
 @inject(HttpClient)
-export class Flickr{
+export class Gallery3{
   heading = 'Flickr';
   images = [];
-  url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=mountain&tagmode=any&format=json';
-
-  constructor(http){
+  selectedImage='';
+  url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=rainier&tagmode=any&format=json';
+    constructor(http){
     this.http = http;
   }
 
+  loadimage(img) {
+    //alert('i '+img);
+    this.selectedImage = img;
+  }
+  changeGallery() {
+    alert('cg')
+
+  }
   activate(){
     return this.http.jsonp(this.url).then(response => {
       this.images = response.content.items;
-    });
-  }
+  });
+
+}
+
+canDeactivate(){
+  return confirm('Are you sure you want to leave?');
+}
 }
